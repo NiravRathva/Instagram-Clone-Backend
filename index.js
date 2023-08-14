1735766472a112905a0ec1fbf8d7c350e70f911c
NiravRathva <niravrathwa57@gmail.com>
2023-08-14 22:52:03 +0530

    connected to database

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-const port = 8800;
+const port = 8000;
+dotenv.config();
+
+//connecting to Database
+const connectToMongo = async () => {
+  await mongoose.connect(process.env.MONGO);
+  console.log("conntected to MongoDB");
+};
 
 app.listen(port, () => {
-  console.log("Server started on port 5000");
+  connectToMongo();
+  console.log(`Server started on port ${port}`);
 });
